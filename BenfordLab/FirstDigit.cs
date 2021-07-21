e69864465a5a5fb2127582a5d921fec50429c32e
prashanthyaddanapudi <65833066+prashanthyaddanapudi@users.noreply.github.com>
2021-07-20 23:17:11 -0400

    1

---
+++ BenfordLab/FirstDigit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLab
+{
+    public class FirstDigit
+    {
+        /* Given an integer X, this method returns the first digit
+         * 
+         * For example,
+         *   5 ==> returns 5
+         *   10 ==> returns 1
+         *   654321 ==> returns 6
+         */
+        public static int getFirstDigit(int digit) {
+            for (; digit >= 10; digit = digit / 10) ;
+            return digit;
+        }    
+    }
+}
